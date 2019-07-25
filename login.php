c1c8cadeaf6c9bea616abd555f14ede35fd0bbaa
Quique <enriquealducin@outlook.com>
2019-07-24 23:50:38 -0500

    Login admin nuevo

---
+++ login.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Volar en Globo</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->  
+  <link rel="icon" type="image/png" href="sources/images/icons/logo.png"/>
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="sources/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="sources/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="sources/fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="sources/vendor/animate/animate.css">
+<!--===============================================================================================-->  
+  <link rel="stylesheet" type="text/css" href="sources/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="sources/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="sources/vendor/select2/select2.min.css">
+<!--===============================================================================================-->  
+  <link rel="stylesheet" type="text/css" href="sources/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="sources/css/util.css">
+  <link rel="stylesheet" type="text/css" href="sources/css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+  
+  
+  <div class="container-login100" style="background-image: url('sources/images/fondos/globos.png');">
+    <div class="wrap-login100 p-l-55 p-r-55 p-t-80 p-b-30" style="opacity: .8">
+      <form class="login100-form validate-form">
+        <span class="login100-form-title p-b-37">
+          Sistema de Administración de Volar en Globo
+        </span>
+
+        <div class="wrap-input100 validate-input m-b-20" data-validate="Ingresa tu Uusarios">
+          <input class="input100" type="text" name="user" placeholder="Ingresa tu Usuario">
+          <span class="focus-input100"></span>
+        </div>
+
+        <div class="wrap-input100 validate-input m-b-25" data-validate = "Ingresa la Contraseña">
+          <input class="input100" type="password" name="pass" placeholder="Contraseña">
+          <span class="focus-input100"></span>
+        </div>
+
+        <div class="container-login100-form-btn">
+          <button class="login100-form-btn">
+            Iniciar Sesión
+          </button>
+        </div>
+
+        <div class="text-center p-t-57 p-b-20">
+          <span class="txt1">
+            <hr>
+          </span>
+        </div>
+
+
+      </form>
+
+      
+    </div>
+  </div>
+  
+  
+
+  <div id="dropDownSelect1"></div>
+  
+<!--===============================================================================================-->
+  <script src="sources/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+  <script src="sources/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+  <script src="sources/vendor/bootstrap/js/popper.js"></script>
+  <script src="sources/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+  <script src="sources/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+  <script src="sources/vendor/daterangepicker/moment.min.js"></script>
+  <script src="sources/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+  <script src="sources/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+  <script src="sources/js/main.js"></script>
+
+</body>
+</html>
