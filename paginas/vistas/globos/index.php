d275852a34b0b1ca29aab4164fe2dde8f4f22f03
Quique <enriquealducin@outlook.com>
2019-08-05 07:17:09 -0500

    modulo de globos terminado

--- paginas/vistas/globos/index.php
+++ paginas/vistas/globos/index.php
@@ -44,25 +44,5 @@
   </div>
 </div>
 
-<div class="modal fade" id="modal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered modal-md" id="modalSize" role="dialog">
-    <div class="modal-content ">
-      <div class="modal-header">
-        <h5 class="modal-title" id="tituloModal"></h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body" id="cuerpoModal">
-        
-      </div>
-      <div class="modal-footer">
-        <div id="DivBtnModal">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-          
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<script type="text/javascript" src="vistas/deptos/js/index.js"></script>
+
+<script type="text/javascript" src="vistas/globos/js/index.js"></script>
--- paginas/vistas/globos/tabla.php
+++ paginas/vistas/globos/tabla.php
@@ -3,8 +3,8 @@
 	require_once  $_SERVER['DOCUMENT_ROOT'].'/admin/paginas/controladores/conexion.php';
 	require_once  $_SERVER['DOCUMENT_ROOT'].'/admin/paginas/controladores/fin_session.php';
 	$modulo= $_POST['modulo'];
-	$campos= "nombre_depto as nombre, id_depto as id";
-	$tabla = "departamentos_volar  ";
+	$campos= "nombre_globo as nombre, id_globo as id,placa_globo as globo ";
+	$tabla = "globos_volar  ";
 	$filtro = "status<>0";
 	$usuario= unserialize((base64_decode($_SESSION['usuario'])));
 	$idUsu=$usuario->getIdUsu();
@@ -12,32 +12,31 @@
 	foreach ($subPermisos as $subPermiso) {
 		$permisos[] = $subPermiso->nombre_sp;
 	}
-	$deptos=$con->consulta($campos,$tabla,$filtro);	
+	$globos=$con->consulta($campos,$tabla,$filtro);	
 ?>
 <table class="DataTable table table-striped table-bordered table-hover">
 	<thead>
 		<tr>
 			<th style="text-align: center;vertical-align: middle;max-width: 1%;width: 1%;">Nombre</th>
+			<th style="text-align: center;vertical-align: middle;max-width: 1%;width: 1%;">Placa</th>
 			
 			<th style="text-align: center;vertical-align: middle;max-width: 1%;width: 1% !important;">Acciones</th>
 		</tr>
 	</thead>
 	<tbody>
 		<?php
-			foreach ($deptos as $depto) {
+			foreach ($globos as $globo) {
 		?>
 			<tr>
-				<td><?php echo $depto->nombre; ?></td>
+				<td><?php echo $globo->nombre; ?></td>
+				<td><?php echo $globo->globo; ?></td>
 				<td>
 					<?php if( in_array("EDITAR", $permisos)){ ?>
-						<i class="fa fa-pencil-square fa-md" style="color:#33b5e5" title="Editar" data-toggle="modal" data-target="#modal"  onclick="accionDeptos('editar', <?php echo $depto->id; ?>)"></i>
+						<i class="fa fa-pencil-square fa-md" style="color:#33b5e5" title="Editar" data-toggle="modal" data-target="#modal"  onclick="accionDeptos('editar', <?php echo $globo->id; ?>)"></i>
 					<?php  } ?>
 					<!--========       Eliminar     ========= -->
 					<?php if( in_array("ELIMINAR", $permisos)){ ?>
-						<i class="fa fa-trash-o fa-md" style="color:#ff4444" title="Eliminar" data-toggle="modal" data-target="#modal"  onclick="eliminarDepto( <?php echo $depto->id; ?>,'<?php echo $depto->nombre;  ?>')" ></i>
-					<?php } ?>
-					<?php if( in_array("PUESTOS", $permisos)){ ?>
-						<i class="fa fa-th-list fa-md" style="color:#aa66cc" title="PUESTOS" data-toggle="modal" data-target="#modal"  onclick="agregarPuestos(<?php echo $depto->id; ?>,'<?php echo $depto->nombre ?>')"></i>
+						<i class="fa fa-trash-o fa-md" style="color:#ff4444" title="Eliminar" data-toggle="modal" data-target="#modal"  onclick="eliminarGlobo( <?php echo $globo->id; ?>,'<?php echo $globo->nombre;  ?>')" ></i>
 					<?php } ?>
 				</td>
 			</tr>
