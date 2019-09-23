c40e2c337c08b9127f0b6e6bb11bbdd8f7526018
sistem23 <sisweb12@dcm.com.mx>
2019-09-23 12:12:23 -0500

    Manual Usuarios

--- cambios enbd.sql
+++ cambios enbd.sql
@@ -1,5 +1,6 @@
 movimientos en BD
 agregar idioma_temp,
 ALTER TABLE `temp_volar` ADD `tipopeso_temp` TINYINT NOT NULL DEFAULT '1' COMMENT 'Tipo de Peso(Kg, lb)' AFTER `kg_temp`;
+ALTER TABLE `temp_volar` ADD `idioma_temp` TINYINT NOT NULL DEFAULT '1' COMMENT 'Idioma' AFTER `hora_temp`;
 
 modificar getReservaData, getReservaREsumen,remplazarReserva, registrarPago
